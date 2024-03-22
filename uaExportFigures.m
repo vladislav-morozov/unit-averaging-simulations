1303ac073af6ac0eb98f4d3d27c556b2a3acca28
vladislav-morozov <86678044+vladislav-morozov@users.noreply.github.com>
2024-03-22 23:07:35 +0100

    New modular plotting approach

--- uaExportFigures.m
+++ uaExportFigures.m
@@ -4,46 +4,112 @@ plotLineThickness= 1.1;
 plotW = 1600; % width in pixels
 plotH = 400; % height in pixels
 
-%% Relative and Absolute MSE
 
-mseSuptitles = {"relative", "absolute"};
-mseStartPlotting = [2, 1];
-for msePlotID = 1:2
+
+%% Line plots
+
+% Relative MSE
+linePlots{1}.data = mseTablesN;
+linePlots{1}.firstColumnPlot = 2;
+linePlots{1}.dataTransform = ...
+    @(dataTable, colIdx) dataTable{:, colIdx}./dataTable{:, 1};
+linePlots{1}.yLims = @(dataTable) [yMinRelMSEPlot, yMaxRelMSEPlot];
+linePlots{1}.relative = true;
+linePlots{1}.suptitle = @(plotDescr) 'Averaging estimator, ' + ...
+                plotDescr + ...
+               ', ratio of MSE to individual estimator';
+linePlots{1}.plotSavingName = 'relative_mse';   
+
+% Absolute MSE
+linePlots{2}.data = mseTablesN;
+linePlots{2}.firstColumnPlot = 1;
+linePlots{2}.dataTransform = ...
+    @(dataTable, colIdx) dataTable{:, colIdx};
+linePlots{2}.yLims = @(dataTable) [0, 1.5*max(dataTable.unrestr)];
+linePlots{2}.relative = false;
+linePlots{2}.suptitle = @(plotDescr) 'Averaging estimator, ' + ...
+                plotDescr + ...
+                ', MSE';
+linePlots{2}.plotSavingName = 'absolute_mse';
+
+% Relative square bias
+linePlots{3}.data = biasTablesN;
+linePlots{3}.firstColumnPlot = 2;
+linePlots{3}.dataTransform = ...
+    @(dataTable, colIdx) (dataTable{:, colIdx}.^2)./(dataTable{:, 1}.^2);
+linePlots{3}.yLims = @(dataTable) [0, ...
+    1.5*max(  ((dataTable.unrestr).^2)./((dataTable{:, 1}.^2)))];
+linePlots{3}.relative = true;
+linePlots{3}.suptitle = @(plotDescr) 'Averaging estimator, ' + ...
+                plotDescr + ...
+                ', relative squared bias';
+linePlots{3}.plotSavingName = 'relative_sq_bias';
+
+% Relative variance
+linePlots{4}.data = varTablesN;
+linePlots{4}.firstColumnPlot = 2;
+linePlots{4}.dataTransform = ...
+    @(dataTable, colIdx) (dataTable{:, colIdx})./(dataTable{:, 1});
+linePlots{4}.yLims = @(dataTable) [0, ...
+    1.5*max(  (dataTable.unrestr./(dataTable{:, 1})))];
+linePlots{4}.relative = true;
+linePlots{4}.suptitle = @(plotDescr) 'Averaging estimator, ' + ...
+                plotDescr + ...
+                ', relative variance';
+linePlots{4}.plotSavingName = 'relative_variance';
+
+
+%%
+ 
+
+% Loop through plot descriptions
+for plotID = 1:length(linePlots) 
+    % Extract description of the plot
+    plotDescrStruct = linePlots{plotID};
+    
     % Loop over parameters
     for parID = 1:numParams
-        
+       
+        % Parameter name
         paramName = paramArray{parID}.saveName;
-        % Loop over N and T
+       
+        % Create figure
         if plotQuietly ~= 1
-            % Plot
-            p = figure('Renderer', 'painters', 'Position', [50 50 plotW plotH]);
+            p = figure('Renderer', 'painters', ...
+                'Position', [50 50 plotW plotH]);
         else
-            p = figure('visible','off', 'Renderer', 'painters', 'Position', [50 50 plotW plotH]);% Do not plot
+            p = figure('visible','off',...
+                'Renderer', 'painters',...
+                'Position', [50 50 plotW plotH]); 
         end
         
+        % Loop over N and T
         for tID = 1:numT
             for nID = 1:numN
+                % Create subplot
                 ax = subplot(numT, numN, (tID-1)*numN+nID);
-                currentParTable = mseTablesN{nID, tID}.(paramName);
-                numApproaches = size(currentParTable, 2);
-                ax.LineStyleOrderIndex = ax.LineStyleOrderIndex; % [1]
-                ax.LineStyleOrder = {'-o','--+','-*','--x','-s','--d','-v','->','-h','-^'};
-                %     ax.ColorOrder = [0 0 0 ];
+                
+                % Extract data
+                currentParTable = plotDescrStruct.data{nID, tID}.(paramName);
+                
+                % Extract number of approaches
+                numApproaches = size(currentParTable, 2); 
+                
+                % Plot
                 hold(ax, 'on');
-                for approachID = mseStartPlotting(msePlotID):numApproaches
+                for approachID = plotDescrStruct.firstColumnPlot:numApproaches
                     %                 if ~plotMethod(approachID)
                     %                    continue
                     %                 end
                     %
                     approach = methodsForPlotting{approachID};
                     approachName = approach.longName;
-                    if msePlotID==1
-                        % Relative MSE
-                        MSE  = currentParTable{:, approachID}./currentParTable{:, 1};
-                    else
-                        MSE  = currentParTable{:, approachID};
-                    end
-                    plot(theta1Range, MSE, ...
+                    
+                    currentLineData = ...
+                        plotDescrStruct.dataTransform(...
+                        currentParTable, approachID);
+
+                    plot(theta1Range, currentLineData, ...
                         'LineStyle', approach.lineStyle, ...
                         'Marker', approach.marker, ...
                         'LineWidth', plotLineThickness, ...
@@ -53,37 +119,30 @@ for msePlotID = 1:2
                     
                 end
                 hold(ax,'off');
+                % Set limits for axes
                 xlim([min(theta1Range), max(theta1Range)])
-                if msePlotID==1
-                    ylim([yMinRelMSEPlot, yMaxRelMSEPlot])
-                else
-                    ylim([0, 1.2*max(currentParTable.unrestr)])
-                end
+                ylim(plotDescrStruct.yLims(currentParTable))
                 
-                h = yline(1);
-                h.HandleVisibility='off';
+                % Add vertical line if appropriate
+                if plotDescrStruct.relative
+                    h = yline(1);
+                    h.HandleVisibility='off';
+                end
                 
                 title("N="+num2str(valuesN(nID))+", T="+num2str(valuesT(tID)))
                 xlabel('\lambda_1')
                 
             end
         end
+        % Add legend
         legend();
-        if msePlotID==1
-           % Absolute MSE
-           suptitle('Averaging estimator, ' + ...
-               paramArray{parID}.plotDescr + ...
-               ', ratio of MSE to individual estimator')
-        else
-            % Relative MSE
-            suptitle('Averaging estimator, ' + ...
-                paramArray{parID}.plotDescr + ...
-                ', MSE')
-        end
-         
+        
+        % Include suptitle
+        suptitle(plotDescrStruct.suptitle(paramArray{parID}.plotDescr))
+       
         % Save figure
         figureSavingName =  figureFolderName + "/" + ...
-            mseSuptitles{msePlotID} + "_" + ...
+            plotDescrStruct.plotSavingName + "_" + ...
             simulationSetting+'_'+paramName +...
             "_N" + titleN + ...
             "_T" + titleT ;
@@ -94,4 +153,12 @@ for msePlotID = 1:2
     end
 end
 
-%% Something
+%% Weights
+
+customColors = [
+    0.2 0.4 0.6;  % Light blue
+    0.1 0.3 0.5;  % Medium blue
+    0.0 0.2 0.4   % Dark blue
+];
+
+
