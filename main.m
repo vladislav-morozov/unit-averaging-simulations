c93871a5dd5a89a09d83a2112b19674f980a1111
vladislav-morozov <86678044+vladislav-morozov@users.noreply.github.com>
2024-11-13 22:57:21 +0100

    Split and reorganize simulation scripts

--- main.m
+++ main.m
@@ -32,10 +32,13 @@ clear variables
 close all
 
 % Load in folders with required functions and scripts
-addpath('utilities')
-addpath('configs') 
+addpath('averagingFunctions') 
+addpath('utilities') % other utilities
+addpath('configs') % Parameters of simulation
+addpath('simulationScriptsFunctions') 
 
 %% Set the parameters of the simulation
+
 % Whether plots should be shown or saved without showing
 plotQuietly = false; 
 
@@ -44,11 +47,10 @@ plotQuietly = false;
 saveWeights = true;
 saveUnrestricted = false; 
  
-
-% Select a seed to drawing coefficients
+% Select a seed to draw data
 seedCoefficients = 1; 
 
-
+% Set number of samples to draw
 
 % Set data generating processes for the simulation
 chooseDGP
@@ -56,113 +58,39 @@ chooseDGP
 % Set parameters of interest
 chooseTargetParameters
 
-% Create unit averaging schemes
-
+% Load in blueprints of unit averaging approaches
+% Note: optimal schemes implemented in createOptimalSchemes function
+averagingApproachBlueprints
  
-%% Averaging schemes
-%
-% NOTE: the generic optimal scheme is used as a blueprint for other optimal
-% schemes. As some large-N schemes depend on the data, these must be created
-% for each given sample. The different schemes are implemented by the
-% uaAddOptimalToMethodsStruct function
-
-methodsArray{1}.weightFunction = ...
-    @(y, covars, estCoefs, estCovars, gradientEstimateTarget, ...
-      targetID, unrestrictedBooltargetId) ...
-            double(1:size(estCoefs, 2) == targetID)';
-methodsArray{1}.shortName = 'ind';        
-methodsArray{1}.longName = 'Individual';
-methodsArray{1}.color = [0.01, 0.01, 0.05]; 
-methodsArray{1}.colorBW = [0.01, 0.01, 0.01];  
-methodsArray{1}.lineStyle = '-';
-methodsArray{1}.marker = 'none';
-methodsArray{1}.markerSize = 6;
-
-
-
-methodsArray{2}.weightFunction = ...
-    @(y, covars, estCoefs, estCovars, gradientEstimateTarget, ...
-      targetID, unrestrictedBooltargetId) ...
-            ones(size(estCoefs, 2), 1)/size(estCoefs, 2);
-methodsArray{2}.shortName = 'mg';        
-methodsArray{2}.longName = 'Mean Group';
-methodsArray{2}.color = [0.7, 0.7, 0.7]; %[141, 111, 100]/255; % brown
-methodsArray{2}.colorBW = [0.79, 0.79, 0.75];
-methodsArray{2}.lineStyle = '-.';
-methodsArray{2}.marker = 'none';
-methodsArray{2}.markerSize = 6;
-
-methodsArray{3}.weightFunction = ...
-    @(y, covars, estCoefs, estCovars, gradientEstimateTarget, ...
-      targetID, unrestrictedBooltargetId) ...
-            uaWeightsAICMMA(estCoefs, y, covars, 'aic');
-methodsArray{3}.shortName = 'aic';        
-methodsArray{3}.longName = 'AIC';
-methodsArray{3}.color =  [0.44, 0.44, 0.4]; % [ 106, 130, 68]/255;
-methodsArray{3}.colorBW = [0.5, 0.5, 0.5];
-methodsArray{3}.lineStyle = '-.';
-methodsArray{3}.marker = '+';
-methodsArray{3}.markerSize = 8;
-
-methodsArray{4}.weightFunction = ...
-    @(y, covars, estCoefs, estCovars, gradientEstimateTarget, ...
-      targetID, unrestrictedBooltargetId) ...
-            uaWeightsAICMMA(estCoefs, y, covars, 'mma');
-methodsArray{4}.shortName = 'mma';        
-methodsArray{4}.longName = 'MMA';
-methodsArray{4}.color =   [ 55, 130, 125]/255; % greenish-teal
-methodsArray{4}.colorBW = [0.01, 0.01, 0.01]; % unplotted in BW
-methodsArray{4}.lineStyle = '-.';
-methodsArray{4}.marker = '.';
-methodsArray{4}.markerSize = 6;
-
-
-methodsArray{5}.weightFunction = ...
-    @(y, covars, estCoefs, estCovars, gradientEstimateTarget, ...
-      targetID, unrestrictedBooltargetId) ...
-        uaWeightsOptimal(estCoefs, estCovars, gradientEstimateTarget, ...
-                         targetID, unrestrictedBooltargetId);
-methodsArray{5}.shortName = 'opt';                   
-methodsArray{5}.longName = 'Generic Optimal'; 
-% The colors and styles should be set for individual units
-methodsArray{5}.color =   [0, 0, 0];
-methodsArray{5}.colorBW = [0, 0, 0];
-methodsArray{5}.lineStyle = '--';
-methodsArray{5}.marker = 'x';
-methodsArray{5}.markerSize = 6;
-
-%% MSE Simulation 
+%% Simulation 1: performance of unit averaging approaches in terms of MSE
 
 % Number of replications
-numReplications = 3333;
+numReplicationsMSE = 33;
 
-for designID = 1:length(simulationSettings)
+for designID = 1:length(simulationSettingsMSE)
     % Extract current design name
-    simulationSetting = simulationSettings(designID);
-    
-    % Close all figures
-    close all
+    simulationSetting = simulationSettingsMSE(designID);
     
     % Set parameters
-    uaSetParameters 
+    setParameters 
     
     % Run simulation
-    uaSimulate
+    simulateMSE
     
     % Export plots 
     uaExportFigures
 end 
 
-%% Coverage simulation
+%% Simulation 2: Performance of confidence intervas
 
 % Number of replications
-numReplications = 500;
+numReplicationsCI = 500;
 
 for designID = 1:length(simulationSettingsCI)
     % Extract current design name
     simulationSetting = simulationSettingsCI(designID);
     
     % Set parameters
-    uaSetParameters
-    uaSimulateCI;
+    setParameters
+    simulateCI;
 end 
--- simulationScriptsFunctions/simulateMSE.m
+++ simulationScriptsFunctions/simulateMSE.m
@@ -1,5 +1,29 @@
+% ===========================================================
+% File: simulateMSE.m
+% Description: This script runs the main simulation for evaluating the MSE
+%   of unit averaging approaches
 
-% Implementation
+% ===========================================================
+%
+% Project Name: Unit Averaging for Heterogeneous Panels
+% Authors: Christian Brownlees, Vladislav Morozov
+%
+% Model: Heterogeneous panel ARX(1) model
+%   y_{it} = theta_{i1} y_{it-1} + theta_{i2} x_{it} + u_{it}
+%
+% Pseudocode for the simulation loop:
+%
+% for (N, T)
+%   for candidate values of (theta_{i1}, theta_{i2})
+%       for sample
+%           draw thetas and set first unit to candidate value
+%           simulate data
+%           estimate parameters
+%           conduct averaging on first unit
+%           record errors of approaches
+%       estimate MSE, bias, and variance for candidate value
+%
+% Implementation notes:
 % Loop through (samples), (N, T) pairs
 % Save three cell arrays with those indices
 % 1. trueTheta array -- hold true values
@@ -11,50 +35,48 @@
 % jth unit being the targe
 % The inloop functions return those deeper levels
 
+% ===========================================================
+%% Extract dimensions of vectors that define the simulation
 
-% for (N, T)
-%   for candidate values of (theta_{i1}, theta_{i2})
-%       for sample
-%           draw thetas and set first unit to candidate value
-%           simulate data
-%           estimate parameters
-%           conduct averaging on first unit
-%           record errors of approaches
-%       estimate MSE value for candidate value
-%
-
-%% Simulation auxiliary computations
-
-% Obtain lengths
+% Numbers of dimensions of samples drawn
 numT = length(valuesT);
 numN = length(valuesN);
-numParams = length(paramArray); % obtain number of parameters used
- 
 
+% Number of target parameters
+numParams = length(paramArray); 
+ 
+% Number of points in the grid for \theta_{i1}
 numTargetPoints = length(theta1Range);
 
+%% Allocate arrays that will hold the results
 
+% MSEs
+mseTablesNT = cell(numN, numT);
+% Biases
+biasTablesNT = cell(numN, numT);
+% Variances
+varTablesNT = cell(numN, numT);
+% All weights
+weightsTablesNT = cell(numN, numT);
+% Weights assigned to first unit
+firstWeightNT = cell(numN, numT);
+% Maximum difference in weights
+maxDiffNT = cell(numN, numT);
+% Difference in mass between restricted and unrestricted sets
+massDiffNT = cell(numN, numT); 
+% Unrestricted units
+unitsUnrestrNT = cell(numN, numT); 
 
 %% Main Loop
-% Loop through parameter vector, draw multiple samples for each
-% value
-
-mseTablesN = cell(numN, numT);
-biasTablesN = cell(numN, numT);
-varTablesN = cell(numN, numT);
-weightsTablesNT = cell(numN, numT);
-firstWeightNT = cell(numN, numT);
-maxDiffNT = cell(numN, numT);
-massDiffNT = cell(numN, numT);
-unitsUnrestrNT = cell(numN, numT);
-
-% Loop through the different values of N
+ 
 for tID = 1:numT
     for nID=1:numN
         % Current sample sizes
         N = valuesN(nID);
         T = valuesT(tID);
         
+        % Create temporary arrays to hold results for each in the grid for
+        % theta_{i1}    
         msePointStructsArray = cell(numTargetPoints, 1);   
         biasPointStructsArray = cell(numTargetPoints, 1);  
         varPointStructsArray = cell(numTargetPoints, 1);  
@@ -63,43 +85,44 @@ for tID = 1:numT
         averageMaxDiffArray = cell(numTargetPoints, 1);
         averageMassDiffArray = cell(numTargetPoints, 1);
         unitsUnresrtArray = cell(numTargetPoints, 1);
+        
         % Iterate through the target values
         for targetValueID = 1:numTargetPoints
             
+            % Extract current target value
             targetValue = theta1Range(targetValueID);
             
             % Create arrays to fill out in the loop
-            errorsArrayTarget = cell(numReplications, 1);
+            errorsArrayTarget = cell(numReplicationsMSE, 1);
             estArrayTarget = errorsArrayTarget;
             targetParamsPoint  = errorsArrayTarget;
             
-            if saveWeights
-                % Save sample and corresponding weights
+            % If weights are to be saved, create suitable arrays to
+            if saveWeights 
                 thetaPointArray = errorsArrayTarget;
                 weightsArray = errorsArrayTarget;
                 unitsUnrestrArray = errorsArrayTarget;
             end
             
             % Draw samples with current target value
-            parfor replID=1:numReplications % parfor this
+            for replID=1:numReplicationsMSE 
                 
                 %%%%%%%%%%%%%%%%%%%%%%%
                 %%% Data Generation %%%
                 %%%%%%%%%%%%%%%%%%%%%%%
                 
- 
-                [thetaTrue, sigmaSq, thetaLabels] = ...
-                        uaDrawCoefficients(...
+                % Draw coefficients
+                [thetaTrue, sigmaSq, thetaClassLabels] = ...
+                        drawCoefficients(...
                             coefApproach, N, ...
                             varNoiseVar,  replID);
                  
-                
-                % Modify the first unit in line with the targetValue
+                % Modify the first unit to have theta_{i1} = targetValue
                 thetaTrue(1, 1) = targetValue;              
                 
                 % Draw data, estimate coefficients and variances
                 [y, covars, u] = ...
-                    uaSimulateData(thetaTrue, sigmaSq, varianceX, ...
+                    drawData(thetaTrue, sigmaSq, varianceX, ...
                     T, replID);
                 
                 % Compute the true values of parameters for unit 1
@@ -122,7 +145,7 @@ for tID = 1:numT
                 % Create optimal strategies for this subsamples
                 optimalSchemes = ...
                     uaOptimalSchemes(...
-                        thetaHat, thetaTrue, thetaLabels, ...
+                        thetaHat, thetaTrue, thetaClassLabels, ...
                         'firstOnly', ...
                         averagingIncludeBool);
                 
@@ -179,9 +202,9 @@ for tID = 1:numT
             end
         end
         % Glue together the msePointStructsArray into a struct of tables
-        [mseTablesN{nID, tID}, ...
-            biasTablesN{nID, tID}, ...
-            varTablesN{nID, tID}] = ...
+        [mseTablesNT{nID, tID}, ...
+            biasTablesNT{nID, tID}, ...
+            varTablesNT{nID, tID}] = ...
             uaProcessMSE(msePointStructsArray, biasPointStructsArray,...
             varPointStructsArray, paramArray);
         
@@ -207,31 +230,34 @@ for tID = 1:numT
         end
     end
 end
-%%
-
+ 
 
 %% Save results
 
+% Close any open figures before exporting results
+close all
+
+% Create file name based on simulation parameters
+% Generate parts of the title that depend on (N, T)
 titleN = "";
 for nID=1:numN
     titleN = titleN + "-" + valuesN(nID) ;
 end
+
 titleT = "";
 for tID=1:numT
     titleT = titleT + "-" + valuesT(tID);
 end
 
+% Create the file title
 fileSaveName =   "Outputs/"+...
     simulationSetting + "/" + ...
-    "Replication-" + num2str(numReplications)+ ...
+    "Replication-" + num2str(numReplicationsMSE)+ ...
     "N" + titleN + ...
     "T" + titleT +...
     "weights" + num2str(saveWeights) + ...
     "unrestricted" + num2str(saveUnrestricted) + ...
     ".mat";
 
-% fileSaveName =   "Outputs/"+num2str(numReplications)+ "etaRange"+...
-%     num2str(min(eta1range))+ "-" + num2str(ceil(max(eta1range)))+ "N"+...
-%     num2str(valuesN(end))+"T"+num2str(T)+".mat";
-
+% Export simulation results
 save(fileSaveName)
