6bea19ae669b0c0be16097a5fe3da046b95b8dc8
Vladislav Morozov <86678044+vladislav-morozov@users.noreply.github.com>
2024-11-29 21:47:49 +0100

    Create setPlottingParameters.m

---
+++ src/configs/setPlottingParameters.m
@@ -0,0 +1,27 @@
+% ===========================================================
+% File: setPlottingParameters.m
+% Description: This script sets parameters related to figure creation
+% ===========================================================
+%
+% Project Name: Unit Averaging for Heterogeneous Panels
+% Developed by: Christian Brownlees, Vladislav Morozov
+% ===========================================================
+
+% Set text interpreters to latex
+set(groot, 'defaultAxesTickLabelInterpreter','latex');
+set(groot, 'defaultLegendInterpreter','latex');
+set(groot, 'defaultTextInterpreter', 'latex');
+
+% Scalar parameters
+plotLineThickness= 1.5;     % Line thickness
+plotWLines = 1000;          % Width of figure in pixels
+plotHLines = 800;           % Height in figure pixels
+
+% Grid of points for line plots. Markers will be placed at positions
+% theta1Range only
+thetaGridMSE = min(theta1Range):0.01:max(theta1Range);
+
+% Axis plotting limits
+yLimsRelMSE.lambda = [0.5, 1.3];     % for the AR(1) parameter
+yLimsRelMSE.beta = [0.9, 1.2];       % for the coefficient on x_{it}
+yLimsRelMSE.forecast = [0.8, 1.3];   % for the forecast parameters
